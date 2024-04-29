af92038c16e21f10c22c615bf4db348015f15ac8
Powvalla <APowvalla26@jesuitmail.org>
2024-04-29 07:52:11 -0700

    tf

--- index.js
+++ index.js
@@ -353,8 +353,8 @@
       render: {
         sprite: {
           texture: `assets/img/${size}.png`,
-          xScale: size / 3.15,
-          yScale: size / 3.15,
+          xScale: size / 12.75,
+          yScale: size / 12.75,
         },
       },
     });
